86e8c76e333a44cada82f1ef08db43619475a044
rytee20 <pin11-afonina-ekaterine@yandex.ru>
2024-04-30 19:05:10 +0300

    added tsble Users_Achievements and new functions: get_user, get_all_achievements, create_achievement, create_user, set_achievements(doesn't work)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from fastapi.responses import JSONResponse
+import datetime
 
 app=FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -20,6 +21,12 @@ class AchievementsBase(BaseModel):
     scores: int
     description: str
 
+class UsersAchievementsBase(BaseModel):
+    id: int
+    id_user: List[UsersBase]
+    id_achievement: List[AchievementsBase]
+    date: datetime
+
 def get_db():
     db=SessionLocal()
     try:
@@ -29,17 +36,38 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get("/")
-async def read_root():
-    return JSONResponse(content={"message": "Hello, World"}, status_code=200)
+@app.get("/users_achievements/users/{id_user}")
+async def get_user(id_user: int, db: db_dependency):
+    result =db.query(models.Users).filter(models.Users.id_user==id_user).first()
+    if not result:
+        raise HTTPException (status_code=404,detail='User is not found')
+    return result
 
-@app.post("/achievements/")
-async def create_achievements(user:UsersBase, achievement:AchievementsBase, db:db_dependency):
+@app.get("/users_achievements/achievements", response_model=List[AchievementsBase])
+async def get_all_achievements(db: db_dependency):
+    result = db.query(models.Achievements).all()
+    if not result:
+        raise HTTPException (status_code=404,detail='Achievements is not found')
+    return result
+
+@app.post("/users_achievements/achievements/create")
+async def create_achievement(achievement:AchievementsBase, db:db_dependency):
     db_achievement=models.Achievements(achievement_name=achievement.achievement_name, scores=achievement.scores, description=achievement.description)
     db.add(db_achievement)
     db.commit()
     db.refresh(db_achievement)
+
+@app.post("/users_achievements/users/create")
+async def create_user(user:UsersBase, db:db_dependency):
     db_user=models.Users(username=user.username, language=user.language)
     db.add(db_user)
     db.commit()
+    db.refresh(db_user)
+
+@app.post("/users_achievements/set_achievement")
+async def set_achievements(u_a:UsersAchievementsBase, db:db_dependency):
+    db_u_a=models.Users(id_user=u_a.id_user, id_achievement=u_a.id_achievement, date=datetime.now())
+    db.add(db_u_a)
+    db.commit()
+    db.refresh(db_u_a)
 
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, DateTime
 from database import Base
 
 class Users(Base):
@@ -15,3 +15,11 @@ class Achievements(Base):
     achievement_name=Column(String, index=True)
     scores=Column(Integer, index=True)
     description=Column(String, index=True)
+
+class Users_Achievements(Base):
+    __tablename__='users_and_their_achievements'
+
+    id=Column(Integer, primary_key=True, index=True)
+    id_user=Column(Integer, ForeignKey("users.id_user"))
+    id_achievement=Column(Integer, ForeignKey("achievements.id_achievement"))
+    date=Column(DateTime, index=True)
