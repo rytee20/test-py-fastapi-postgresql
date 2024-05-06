eb8c22ddeba15c43d0451b65ac79654fcf43cae2
rytee20 <pin11-afonina-ekaterine@yandex.ru>
2024-05-06 19:04:30 +0300

    добавлены комментарии

--- main.py
+++ main.py
@@ -13,22 +13,25 @@ from sqlalchemy import case
 app=FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+# Таблица с данными о пользователях
 class UsersBase(BaseModel):
-    id_user: int
-    username: str
-    language: str
+    id_user: int                            # Идентификатор
+    username: str                           # Имя пользователя
+    language: str                           # Язык
 
+# Таблица достижений
 class AchievementsBase(BaseModel):
-    id_achievement: int
-    achievement_name: str
-    scores: int
-    description: str
+    id_achievement: int                     # Идентиффикатор
+    achievement_name: str                   # Наименование достижения
+    scores: int                             # Количество очков
+    description: str                        # Описание достижения
 
+# Таблица выдачи достижений пользователям
 class UsersAchievementsBase(BaseModel):
-    id: int
-    id_user: List[UsersBase]
-    id_achievement: List[AchievementsBase]
-    date: datetime
+    id: int                                 # Идентификато
+    id_user: List[UsersBase]                # Идентификатор пользователя
+    id_achievement: List[AchievementsBase]  # Идентификатор достижения
+    date: datetime                          # Дата выдачи
 
 class Language(str, Enum):
     ru = "ru"
@@ -41,31 +44,42 @@ def get_db():
     finally:
         db.close()
 
-db_dependency = Annotated[Session, Depends(get_db)]
-
+# Предоставление данных о пользователе
 @app.get("/users_achievements/users/{id_user}")
-async def get_user(id_user: int, db: db_dependency):
+async def get_user(id_user: int, db: Session = Depends(get_db)):
     result = db.query(models.Users).filter(models.Users.id_user==id_user).first()
+
     if not result:
         raise HTTPException (status_code=404,detail='User is not found')
+    
     return result
 
+# Предоставление данных о всех возможных достижениях
 @app.get("/users_achievements/achievements", response_model=List[AchievementsBase])
-async def get_all_achievements(db: db_dependency):
+async def get_all_achievements(db: Session = Depends(get_db)):
     result = db.query(models.Achievements).all()
+
     if not result:
         raise HTTPException (status_code=404,detail='Achievements is not found')
+    
     return result
 
+# Предоставления данных о достижения конкретного пользователя на его языке
 @app.get("/users_achievements/users/{id_user}/achievements")
-async def get_users_achievements(id_user: int, db: db_dependency):
-    result = db.query(models.Achievements).join(models.Users_Achievements).filter(models.Users_Achievements.id_user==id_user).all()
+async def get_users_achievements(id_user: int, db: Session = Depends(get_db)):
+    #Достижения пользователя
+    result = (db.query(models.Achievements)
+              .join(models.Users_Achievements)
+              .filter(models.Users_Achievements.id_user==id_user)
+              .all())
 
     if not result:
         raise HTTPException (status_code=404,detail='Achievements is not found')
 
+    # Данные пользователя
     user = db.query(models.Users).filter(models.Users.id_user == id_user).first()
 
+    # Данные о достижениях с переводом на язык пользователя
     translated_achievements = []
     for achievement in result:
         translated_achievement = {
@@ -78,14 +92,22 @@ async def get_users_achievements(id_user: int, db: db_dependency):
 
     return translated_achievements
 
+# Предоставление данных о пользователях с максимальным количеством достижений
 @app.get("/users_achievements/users_with_max_achievements")
-async def get_users_with_max_achievements(db: db_dependency):
-    user_achievements_count = db.query(models.Users_Achievements.id_user, func.count().label("achievements_count")).group_by(models.Users_Achievements.id_user).subquery()
+async def get_users_with_max_achievements(db: Session = Depends(get_db)):
+    # Подсчет достижений каждого пользователя
+    user_achievements_count = (db.query(models.Users_Achievements.id_user, func.count().label("achievements_count"))
+                               .group_by(models.Users_Achievements.id_user)
+                               .subquery())
 
-    max_achievements_count = db.query(
-        func.max(user_achievements_count.c.achievements_count)).scalar()
+    # Максимальное количество достижений
+    max_achievements_count = db.query(func.max(user_achievements_count.c.achievements_count)).scalar()
 
-    users_with_max_achievements = db.query(models.Users).join(user_achievements_count, models.Users.id_user == user_achievements_count.c.id_user).filter(user_achievements_count.c.achievements_count == max_achievements_count).all()
+    # Пользователи с количеством достижений равным максимальному
+    users_with_max_achievements = (db.query(models.Users)
+                                   .join(user_achievements_count, models.Users.id_user == user_achievements_count.c.id_user)
+                                   .filter(user_achievements_count.c.achievements_count == max_achievements_count)
+                                   .all())
 
     if not users_with_max_achievements:
         raise HTTPException (status_code=404,detail='Users is not found')
@@ -101,21 +123,23 @@ async def get_users_with_max_achievements(db: db_dependency):
 
     return result
 
+# Предоставление данных о пользователях с максимальным количеством очков
 @app.get("/users_achievements/users_with_max_scores")
-async def get_users_with_max_scores(db: db_dependency):
-    user_scores = (
+async def get_users_with_max_scores(db: Session = Depends(get_db)):
+    # Подсчет очков каждого пользователя
+    user_scores_count = (
         db.query(
             models.Users_Achievements.id_user,
-            func.sum(models.Achievements.scores).label("total_scores")
-        )
+            func.sum(models.Achievements.scores).label("total_scores"))
         .join(models.Achievements, models.Users_Achievements.id_achievement == models.Achievements.id_achievement)
         .group_by(models.Users_Achievements.id_user)
-        .subquery()
-    )
+        .subquery())
 
-    max_scores = db.query(func.max(user_scores.c.total_scores)).scalar()
+    # Максимальное количество очков
+    max_scores = db.query(func.max(user_scores_count.c.total_scores)).scalar()
 
-    users_with_max_scors = db.query(models.Users).join(user_scores, models.Users.id_user == user_scores.c.id_user).filter(user_scores.c.total_scores == max_scores).all()
+    # Пользователи с количеством очков равным максимальному
+    users_with_max_scors = db.query(models.Users).join(user_scores_count, models.Users.id_user == user_scores_count.c.id_user).filter(user_scores_count.c.total_scores == max_scores).all()
 
     if not users_with_max_scors:
         raise HTTPException (status_code=404,detail='Users is not found')
@@ -131,23 +155,26 @@ async def get_users_with_max_scores(db: db_dependency):
     
     return result
 
+# Предоставление данных о пользователях с максимальной разницей очков достижений
 @app.get("/users_achievements/users_with_max_difference")
-async def get_users_with_max_difference(db: db_dependency):
+async def get_users_with_max_difference(db: Session = Depends(get_db)):
+    # Максимальное возможное число очков достижений (сумма очков всех возможных достижений)
     max_scores=db.query(func.sum(models.Achievements.scores)).scalar()
 
+    # Данные о количестве очков пользователей и разничей с максимальным возможным значением очков
     user_scores = (
         db.query(
             models.Users_Achievements.id_user,
             func.sum(models.Achievements.scores).label("total_scores"),
-            (max_scores-func.sum(models.Achievements.scores)).label("difference")
-        )
+            (max_scores-func.sum(models.Achievements.scores)).label("difference"))
         .join(models.Achievements, models.Users_Achievements.id_achievement == models.Achievements.id_achievement)
         .group_by(models.Users_Achievements.id_user)
-        .subquery()
-    )
+        .subquery())
 
+    # Максимальная разница
     max_difference=db.query(func.max(user_scores.c.difference)).scalar()
 
+    # Пользователи с максимальной разницей
     users_with_max_diff=(
         db.query(user_scores.c.id_user, models.Users.username, user_scores.c.total_scores, user_scores.c.difference)
         .filter(models.Users.id_user==user_scores.c.id_user)
@@ -162,23 +189,26 @@ async def get_users_with_max_difference(db: db_dependency):
 
     return result
 
+# Предоставление данных о пользователях с минимальной разницей очков достижений
 @app.get("/users_achievements/users_with_min_difference")
-async def get_users_with_min_difference(db: db_dependency):
+async def get_users_with_min_difference(db: Session = Depends(get_db)):
+    # Максимальное возможное число очков достижений (сумма очков всех возможных достижений)
     max_scores=db.query(func.sum(models.Achievements.scores)).scalar()
 
+    # Данные о количестве очков пользователей и разничей с максимальным возможным значением очков
     user_scores = (
         db.query(
             models.Users_Achievements.id_user,
             func.sum(models.Achievements.scores).label("total_scores"),
-            (max_scores-func.sum(models.Achievements.scores)).label("difference")
-        )
+            (max_scores-func.sum(models.Achievements.scores)).label("difference"))
         .join(models.Achievements, models.Users_Achievements.id_achievement == models.Achievements.id_achievement)
         .group_by(models.Users_Achievements.id_user)
-        .subquery()
-    )
+        .subquery())
 
+    # Минимальная разница
     min_difference=db.query(func.min(user_scores.c.difference)).scalar()
 
+    # Пользователи с минимальной разницей
     users_with_max_diff=(
         db.query(user_scores.c.id_user, models.Users.username, user_scores.c.total_scores, user_scores.c.difference)
         .filter(models.Users.id_user==user_scores.c.id_user)
@@ -192,37 +222,36 @@ async def get_users_with_min_difference(db: db_dependency):
               for id_user, username, total_scores, difference in users_with_max_diff]
 
     return result
-
+# Предоставление данных о пользователях, которые за последние 7 дней каждый день получали хотя бы одно достижение
 @app.get("/users_achievements/users_with_7days_achievements")
-async def get_users_with_7days_achievements(db: db_dependency):
-
+async def get_users_with_7days_achievements(db: Session = Depends(get_db)):
+    # Текущая дата
     today_date = datetime.now()
 
-    get_users_count_achievements = (
+    # Данные о пользователи и дате полученных ими достижений за последние 7 дней
+    get_users_date_achievements = (
         db.query(
             models.Users_Achievements.id_user,
-            func.date(models.Users_Achievements.date).label("date")
-        )
+            func.date(models.Users_Achievements.date).label("date"))
         .filter(models.Users_Achievements.date >= today_date - timedelta(days=6))
         .filter(models.Users_Achievements.date <= today_date)
-        .subquery()
-    )
+        .subquery())
 
+    # Данные о пользователе и количестве полученных им достижений за каждый день в течении последней недели
     get_users_count_7days_achievements = (
         db.query(
-            get_users_count_achievements.c.id_user,
-            func.sum(case((func.date(get_users_count_achievements.c.date) == func.date(today_date - timedelta(days=6)), 1),else_ =0)).label("day1"),
-            func.sum(case((func.date(get_users_count_achievements.c.date) == func.date(today_date - timedelta(days=5)), 1),else_ =0)).label("day2"),
-            func.sum(case((func.date(get_users_count_achievements.c.date) == func.date(today_date - timedelta(days=4)), 1),else_ =0)).label("day3"),
-            func.sum(case((func.date(get_users_count_achievements.c.date) == func.date(today_date - timedelta(days=3)), 1),else_ =0)).label("day4"),
-            func.sum(case((func.date(get_users_count_achievements.c.date) == func.date(today_date - timedelta(days=2)), 1),else_ =0)).label("day5"),
-            func.sum(case((func.date(get_users_count_achievements.c.date) == func.date(today_date - timedelta(days=1)), 1),else_ =0)).label("day6"),
-            func.sum(case((func.date(get_users_count_achievements.c.date) == func.date(today_date), 1),else_ =0)).label("day7"),
-        )
-        .group_by(get_users_count_achievements.c.id_user)
-        .subquery()
-    )
+            get_users_date_achievements.c.id_user,
+            func.sum(case((func.date(get_users_date_achievements.c.date) == func.date(today_date - timedelta(days=6)), 1),else_ =0)).label("day1"),
+            func.sum(case((func.date(get_users_date_achievements.c.date) == func.date(today_date - timedelta(days=5)), 1),else_ =0)).label("day2"),
+            func.sum(case((func.date(get_users_date_achievements.c.date) == func.date(today_date - timedelta(days=4)), 1),else_ =0)).label("day3"),
+            func.sum(case((func.date(get_users_date_achievements.c.date) == func.date(today_date - timedelta(days=3)), 1),else_ =0)).label("day4"),
+            func.sum(case((func.date(get_users_date_achievements.c.date) == func.date(today_date - timedelta(days=2)), 1),else_ =0)).label("day5"),
+            func.sum(case((func.date(get_users_date_achievements.c.date) == func.date(today_date - timedelta(days=1)), 1),else_ =0)).label("day6"),
+            func.sum(case((func.date(get_users_date_achievements.c.date) == func.date(today_date), 1),else_ =0)).label("day7"),)
+        .group_by(get_users_date_achievements.c.id_user)
+        .subquery())
 
+    # Пользователи, получившие за каждый день хотя бы одно достижение
     get_users_with_7days_achievements = (
         db.query(
             get_users_count_7days_achievements.c.id_user,
@@ -232,22 +261,19 @@ async def get_users_with_7days_achievements(db: db_dependency):
                         get_users_count_7days_achievements.c.day4 != 0 and
                         get_users_count_7days_achievements.c.day5 != 0 and
                         get_users_count_7days_achievements.c.day6 != 0 and
-                        get_users_count_7days_achievements.c.day7 != 0, 1), else_=0)
-            ).label("achievements_in_sequence")
-        )
+                        get_users_count_7days_achievements.c.day7 != 0, 1), else_=0))
+                        .label("achievements_in_sequence"))
         .group_by(get_users_count_7days_achievements.c.id_user)
-        .subquery()
-    )
+        .subquery())
 
+    # Результат (is_user и username)
     users = (
         db.query(
             get_users_with_7days_achievements.c.id_user,
-            models.Users.username
-        )
+            models.Users.username)
         .filter(get_users_with_7days_achievements.c.id_user==models.Users.id_user)
         .filter(get_users_with_7days_achievements.c.achievements_in_sequence!=0)
-        .all()
-    )
+        .all())
 
     if not users:
         raise HTTPException (status_code=404,detail='Users is not found')
@@ -257,25 +283,39 @@ async def get_users_with_7days_achievements(db: db_dependency):
 
     return result
 
-
+# Добавление достижения
 @app.post("/users_achievements/achievements/create")
-async def create_achievement(achievement_name:str, scores:int, description:str, db:db_dependency):
+async def create_achievement(achievement_name:str, scores:int, description:str, db:Session = Depends(get_db)):
     db_achievement = models.Achievements(achievement_name=achievement_name, scores=scores, description=description)
     db.add(db_achievement)
     db.commit()
     db.refresh(db_achievement)
 
+# Добавления нового пользователя
 @app.post("/users_achievements/users/create")
-async def create_user(db: db_dependency, username: str, language: Language):
+async def create_user(username: str, language: Language, db: Session = Depends(get_db)):
     db_user = models.Users(username=username, language=language.value)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
 
+# Присвоение достижения пользователю
 @app.post("/users_achievements/set_achievement")
-async def set_achievement(id_user:int, id_achievement:int, db:db_dependency):
-    db_u_a = models.Users_Achievements(id_user=id_user, id_achievement=id_achievement)
-    db.add(db_u_a)
-    db.commit()
-    db.refresh(db_u_a)
+async def set_achievement(id_user:int, id_achievement:int, db: Session = Depends(get_db)):
+    # Проверяем есть ли у пользователя данное достижение
+    user_with_such_achievement = (
+        db.query(
+            models.Users_Achievements.id_user,
+            models.Users_Achievements.id_achievement)
+        .filter(models.Users_Achievements.id_user==id_user)
+        .filter(models.Users_Achievements.id_achievement==id_achievement)
+        .all())
+    
+    if not user_with_such_achievement:
+        db_u_a = models.Users_Achievements(id_user=id_user, id_achievement=id_achievement)
+        db.add(db_u_a)
+        db.commit()
+        db.refresh(db_u_a)
+    else: 
+        raise HTTPException (status_code=409,detail='The user already has this achievement')
 
--- models.py
+++ models.py
@@ -2,25 +2,28 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from database import Base
 from datetime import datetime
 
+# Таблица с данными о пользователях
 class Users(Base):
     __tablename__='users'
 
-    id_user=Column(Integer, primary_key=True, index=True)
-    username=Column(String, index=True)
-    language=Column(String, index=True)
+    id_user=Column(Integer, primary_key=True, index=True)                       # Идентификатор
+    username=Column(String, index=True)                                         # Имя пользователя
+    language=Column(String, index=True)                                         # Язык
 
+# Таблица достижений
 class Achievements(Base):
     __tablename__='achievements'
 
-    id_achievement=Column(Integer, primary_key=True, index=True)
-    achievement_name=Column(String, index=True)
-    scores=Column(Integer, index=True)
-    description=Column(String, index=True)
+    id_achievement=Column(Integer, primary_key=True, index=True)                # Идентификатор
+    achievement_name=Column(String, index=True)                                 # Наименование достижения
+    scores=Column(Integer, index=True)                                          # Количество очков
+    description=Column(String, index=True)                                      # Описание достижения
 
+# Таблица выдачи достижений пользователям
 class Users_Achievements(Base):
     __tablename__='users_and_their_achievements'
 
-    id=Column(Integer, primary_key=True, index=True)
-    id_user=Column(Integer, ForeignKey("users.id_user"))
-    id_achievement=Column(Integer, ForeignKey("achievements.id_achievement"))
-    date=Column(DateTime, default=datetime.now())
+    id=Column(Integer, primary_key=True, index=True)                            # Идентификатор
+    id_user=Column(Integer, ForeignKey("users.id_user"))                        # Идентификатор пользователя
+    id_achievement=Column(Integer, ForeignKey("achievements.id_achievement"))   # Идентификатор достижения
+    date=Column(DateTime, default=datetime.now())                               # Дата выдачи (по умолчания дата внесения данных в таблицу)
