2cb9691b607d552d1d8b819f3ecb6f15a9c2f623
Ekaterina <94058290+rytee20@users.noreply.github.com>
2024-05-06 21:59:05 +0300

    Добавлены комментарии

--- database.py
+++ database.py
@@ -2,10 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://postgres:qwerty22@localhost:5432/users_achievements'
+# Подключение к базе данных
+URL_DATABASE = 'postgresql://postgres:password@localhost:5432/users_achievements'
 
+# Движок SQLAlchemy для управления подключением к базе данных
 engine = create_engine(URL_DATABASE)
 
+# Сессия SQLAlchemy для взаимодействия с базой данных
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Базовый класс для определения моделей SQLAlchemy
 Base = declarative_base()
